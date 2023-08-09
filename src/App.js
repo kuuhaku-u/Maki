249ecb00867c5cd663040257057eb1acb6b21ed7
Ravipyasi <ravipdev07@gmail.com>
2023-08-09 10:40:36 +0530

    fix(src): Some fixes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import { Fragment } from 'react';
 import './App.css';
 import Login from './components/login';
 import Registration from './components/registeration';
+import Test from './components/Test';
+import { Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <Fragment>
-      <Registration />
-      <Login />
-    </Fragment>
+    <Routes>
+      <Route path="/" element={<Registration />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/home" element={<Test />}></Route>
+      {/* <Fragment>
+        <Registration />
+        <Login />
+      </Fragment> */}
+    </Routes>
   );
 }
 
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import { register } from './registerationStyles';
@@ -14,76 +14,115 @@ import ErrorIcon from '@material-ui/icons/Error';
 import VisibilityTwoToneIcon from '@material-ui/icons/VisibilityTwoTone';
 import VisibilityOffTwoToneIcon from '@material-ui/icons/VisibilityOffTwoTone';
 import CloseIcon from '@material-ui/icons/Close';
-class Registration extends Component {
-  state = {
-    email: '',
-    password: '',
-    passwordConfrim: '',
-    hidePassword: true,
-    error: null,
-    errorOpen: false,
+import { useNavigate } from 'react-router-dom';
+import Protected from './Protected';
+import Test from './Test';
+
+const Registration = ({classes}) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfrim, setPasswordConfrim] = useState("");
+  const [hidePassword, setHidePassword] = useState(true);
+  const [error, setError] = useState(null);
+  const [errorOpen, setErrorOpen] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  const navigate = useNavigate();
+  // state = {
+  //   email: '',
+  //   password: '',
+  //   passwordConfrim: '',
+  //   hidePassword: true,
+  //   error: null,
+  //   errorOpen: false,
+  //   isLoggedIn: false
+  // };
+  const errorClose = (e) => {
+    setErrorOpen(false);
+    // this.setState({
+    //   errorOpen: false,
+    // });
   };
-  errorClose = (e) => {
-    this.setState({
-      errorOpen: false,
-    });
+
+  const handleChange = (name) => (e) => {
+    if(name == "password"){
+      setPassword(e.target.value);
+    } 
+    if(name == "email"){
+      setEmail(e.target.value);
+    }
+    // this.setState({
+    //   [name]: e.target.value,
+    // });
   };
-  handleChange = (name) => (e) => {
-    this.setState({
-      [name]: e.target.value,
-    });
+  const passwordMatch = () => true;
+  const showPassword = () => {
+    // this.setState((prevState) => ({ hidePassword: !prevState.hidePassword }));
+    setHidePassword((prev) => !prev);
   };
-  passwordMatch = () => true;
-  showPassword = () => {
-    this.setState((prevState) => ({ hidePassword: !prevState.hidePassword }));
-  };
-  isValid = () => {
-    if (this.state.email === '') {
+  const isValid = () => {
+    if (email === '') {
       return false;
     }
     return true;
   };
-  submitRegistration = (e) => {
+  const submitRegistration = (e) => {
     e.preventDefault();
-    if (!this.passwordMatch()) {
-      this.setState({
-        errorOpen: true,
-        error: "Passwords don't match",
-      });
+    if (!passwordMatch()) {
+      setIsLoggedIn(false);
+      setErrorOpen(true);
+      setError("Passwords don't match")
+      // this.setState({
+      //   isLoggedIn: false,
+      //   errorOpen: true,
+      //   error: "Passwords don't match",
+      // });
+    } else{
+      // this.setState({
+      //   isLoggedIn: true
+      // });
+      setIsLoggedIn(true);
     }
-    const newUserCredentials = {
-      email: this.state.email,
-      password: this.state.password,
-      passwordConfrim: this.state.passwordConfrim,
-    };
-    console.log(document.cookie);
-    fetch('http://localhost:8000/login', {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${document.cookie} `,
-      },
-      body: JSON.stringify({
-        username: newUserCredentials.email,
-        password: newUserCredentials.password,
-      }),
-    })
-      .then((e) => e.json())
-      .then((e) => (document.cookie = e.token));
+    // setIsLoggedIn(true);
+    // const newUserCredentials = {
+    //   email: this.state.email,
+    //   password: this.state.password,
+    //   passwordConfrim: this.state.passwordConfrim,
+    // };
+    // console.log(document.cookie);
+    // fetch('http://localhost:8000/login', {
+    //   method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    //   mode: 'cors', // no-cors, *cors, same-origin
+    //   cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    //   credentials: 'same-origin', // include, *same-origin, omit
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'Authorization': `Bearer ${document.cookie} `,
+    //   },
+    //   body: JSON.stringify({
+    //     username: newUserCredentials.email,
+    //     password: newUserCredentials.password,
+    //   }),
+    // })
+    //   .then((e) => e.json())
+    //   .then((e) => (document.cookie = e.token));
   };
-  render() {
-    const { classes } = this.props;
-    return (
+
+  useEffect(() => {
+    if(isLoggedIn){
+      navigate("/home");
+    }
+  }, [isLoggedIn]);
+
+  return (
+    <>
       <div className={classes.main}>
         <CssBaseline />
         <Paper className={classes.paper}>
           <Avatar className={classes.avatar}>
             <PeopleAltIcon className={classes.icon} />
           </Avatar>
-          <form className={classes.form} onSubmit={() => this.submitRegistration}>
+          <form className={classes.form} onSubmit={() => submitRegistration}>
             <FormControl required fullWidth margin='normal'>
               <InputLabel htmlFor='email' className={classes.labels}>
                 User name
@@ -94,7 +133,7 @@ class Registration extends Component {
                 autoComplete='email'
                 className={classes.inputs}
                 disableUnderline={true}
-                onChange={this.handleChange('email')}
+                onChange={handleChange('email')}
               />
             </FormControl>
             <FormControl required fullWidth margin='normal'>
@@ -106,15 +145,15 @@ class Registration extends Component {
                 autoComplete='password'
                 className={classes.inputs}
                 disableUnderline={true}
-                onChange={this.handleChange('password')}
-                type={this.state.hidePassword ? 'password' : 'input'}
+                onChange={handleChange('password')}
+                type={hidePassword ? 'password' : 'input'}
                 endAdornment={
-                  this.state.hidePassword ? (
+                  hidePassword ? (
                     <InputAdornment position='end'>
                       <VisibilityOffTwoToneIcon
                         fontSize='default'
                         className={classes.passwordEye}
-                        onClick={this.showPassword}
+                        onClick={showPassword}
                       />
                     </InputAdornment>
                   ) : (
@@ -122,7 +161,7 @@ class Registration extends Component {
                       <VisibilityTwoToneIcon
                         fontSize='default'
                         className={classes.passwordEye}
-                        onClick={this.showPassword}
+                        onClick={showPassword}
                       />
                     </InputAdornment>
                   )
@@ -130,26 +169,26 @@ class Registration extends Component {
               />
             </FormControl>
             <Button
-              disabled={!this.isValid()}
+              disabled={!isValid()}
               disableRipple
               fullWidth
               variant='outlined'
               className={classes.button}
               type='submit'
-              onClick={this.submitRegistration}>
+              onClick={submitRegistration}>
               Login{' '}
             </Button>
           </form>
-          {this.state.error ? (
+          {error ? (
             <Snackbar
               variant='error'
-              key={this.state.error}
+              key={error}
               anchorOrigin={{
                 vertical: 'bottom',
                 horizontal: 'center',
               }}
-              open={this.state.errorOpen}
-              onClose={this.errorClose}
+              open={errorOpen}
+              onClose={errorClose}
               autoHideDuration={3000}>
               <SnackbarContent
                 className={classes.error}
@@ -158,11 +197,11 @@ class Registration extends Component {
                     <span style={{ marginRight: '8px' }}>
                       <ErrorIcon fontSize='large' color='error' />
                     </span>
-                    <span> {this.state.error} </span>
+                    <span> {error} </span>
                   </div>
                 }
                 action={[
-                  <IconButton key='close' aria-label='close' onClick={this.errorClose}>
+                  <IconButton key='close' aria-label='close' onClick={errorClose}>
                     <CloseIcon color='error' />
                   </IconButton>,
                 ]}
@@ -171,8 +210,11 @@ class Registration extends Component {
           ) : null}
         </Paper>
       </div>
+      {isLoggedIn && <Protected>
+          <Test></Test>
+        </Protected>}
+      </>
     );
-  }
 }
 export default withStyles(register)(Registration);
 
